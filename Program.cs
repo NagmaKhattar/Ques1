931beae05e4dab040bcf1eb7542b0440c06fbaef
Nagma <99428751+NagmaKhattar@users.noreply.github.com>
2022-04-25 19:29:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+		// create instance of a new DocumentManager
+		var dm = new DocumentManager<int>();
+
+		// add documents to the DocumentManager
+		dm.AddDocument(1);
+		dm.AddDocument(2);
+
+		dm.IsDocumentAvailable(0);
+
+
+
+	public class DocumentManager<TDocument>
+		where TDocument : struct
+	{
+		private readonly List<TDocument> documentList = new List<TDocument>();
+
+		public void AddDocument(TDocument doc)
+		{
+			lock (this)
+			{
+				documentList.Add(doc);
+			}
+		}
+
+		public TDocument IsDocumentAvailable(TDocument doc)
+
+		{
+
+			return documentList[0];
+
+		}
+		public void Descending(TDocument doc)
+		{
+
+			documentList.Sort();
+			documentList.Reverse();
+
+		}
+
+
+
+
+	}
